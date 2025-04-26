aa92d24bc7167301336102b3b190b0f50f86c663
fredatwox <freddiecom10@gmail.com>
2025-04-26 11:40:56 +0000

    cors for vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://recipe-website-6hfz.vercel.app'],
   credentials: true,
 }));
 
